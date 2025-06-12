ebe4f4466a617f212f71e164a79290f70b3527d8
Rodrigo Reséndez <alexresendezt@gmail.com>
2025-06-11 18:10:59 -0600

    Corrijo archivo app.py como Python válido

--- app.py
+++ app.py
@@ -53,4 +53,5 @@ def login():
         return jsonify({'error': 'Credenciales incorrectas'}), 401
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
